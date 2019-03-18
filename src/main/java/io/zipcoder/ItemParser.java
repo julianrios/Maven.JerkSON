395f3d665d45f79f143c7b1db96aa066e5b58806
Julian Rios <julianrios@J-No.localdomain>
2019-03-18 16:14:45 -0400

    passed single item tests

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -9,37 +9,40 @@ import java.util.regex.Pattern;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
+
         return null;
     }
 
-    public Item parseSingleItem(String singleItem) throws ItemParseException {
+    public Item parseSingleItem(String singleItem) throws ItemParseException, IllegalArgumentException {
         singleItem = singleItem.toLowerCase();
-//          1   2    3     4    5    6     7           8
-//        naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##
-        String pattern = ("(\\w+):(\\w+);(\\w+):(\\d\\.\\d{2});(\\w+):(\\w+);(\\w+):((\\d{1,2}/\\d{1,2}/\\d{4}))##");
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(singleItem);
-        m.matches();
-//        System.out.println(m.matches()); // returns true if it is a pattern
-//        System.out.println(m.group(0)); // returns the entire matching pattern
-//        System.out.println(m.group(2));
-//        System.out.println(m.group(4));
-//        System.out.println(m.group(6));
-//        System.out.println(m.group(8));
 
-        String name = m.group(2);
-        Double price = Double.parseDouble(m.group(4));
-        String food = m.group(6);
-        String expiration = m.group(8);
+        String pattern = ("(\\w+)[%^*!@:;]+(\\w+);(\\w+)[%^*!@:;]+(\\d+\\.\\d{1,2})[%^*!@:;]+(\\w+)[%^*!@:;]+(\\w+);(\\w+)[%^*!@:;]+((\\d{1,2}/\\d{1,2}/\\d{4}))##");
+
+        Pattern p = Pattern.compile(pattern);
+
+        Matcher m = p.matcher(singleItem);
+
+        System.out.println(m.matches());
+
+        String name;
+        Double price;
+        String food;
+        String expiration;
+
+        name = m.group(2);
+        price = matchPrice(m.group(4));
+        food = m.group(6);
+        expiration = m.group(8);
 
         return new Item(name, price, food, expiration);
     }
 
-    public String name(String name) {
-        if(name.equals("Milk")) {
 
-        } else if(name.equals("Milk"))
+    public Double matchPrice(String price) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile("(\\d+\\.\\d{1,2})");
+        if (!pattern.matcher(price).matches()) {
+            throw new IllegalArgumentException();
+        }
+        return Double.parseDouble(price);
     }
-
-
 }
